8b3e0411cbe4bf40b8adec9e731f5b4188907545
junaidmsm <mjunaid@live.co.uk>
2023-05-21 12:40:49 +0100

    changes to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,17 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
+
 
 export const CREATE_VOTE = gql`
   mutation createVote($_id: String!, $techNum: Int!) {
