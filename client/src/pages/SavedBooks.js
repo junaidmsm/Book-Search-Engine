9ac1d976deb1f5a5456345a7aefa846df9c5b04e
junaidmsm <mjunaid@live.co.uk>
2023-06-05 12:59:31 +0100

    changes to js files

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -54,10 +54,7 @@ const SavedBooks = () => {
         <h2>
         {userData?.savedBooks?.length
             ? `Viewing ${userData?.savedBooks?.length} saved ${userData?.savedBooks?.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            : 'You have no saved books!'}          
         </h2>
         <CardColumns>
           {userData.savedBooks.map((book) => {
