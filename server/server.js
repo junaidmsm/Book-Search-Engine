4bd7cb5c78139f00eb29ba00faa4b696bec6e110
junaidmsm <mjunaid@live.co.uk>
2023-06-11 17:11:26 +0100

    changes to server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const db = require('./config/connection');
 
 //connect to MongodDB
 const MONGODB_URI = process.env.MONGOATLAS
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name'); ?
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name');
 
 
 //import our typeDefs and resolvers
