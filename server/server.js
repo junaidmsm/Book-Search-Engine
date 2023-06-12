b841d962d395589ff1c772d9db58da6a0220ca76
junaidmsm <mjunaid@live.co.uk>
2023-06-12 13:08:37 +0100

    changes to server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const db = require('./config/connection');
 
 //connect to MongodDB
 const MONGODB_URI = process.env.MONGOATLAS
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Project0');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Cluster0');
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
